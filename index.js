d725a449245990975cc788e25cb19a391c93d4f7
Aggrey Maphutha <maphuthaaggrey83@gmail.com>
2024-11-13 01:09:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -45,9 +45,19 @@ window.addEventListener('scroll', function() {
 });
 
 
+
+
+
+
+
+
+
 const menu = document.querySelector('nav ul');
 const menuBtn = document.querySelector('.menu-icon');
 const closeBtn = document.querySelector('.close-btn');
+const contact = document.querySelector('.footer-cont');
+const contactBtn = document.querySelector('.contactButton');
+const cancelBtn = document.querySelector('.cancelBtn');
 
 menuBtn.addEventListener('click', () => {
     menu.classList.add('open')
@@ -55,6 +65,19 @@ menuBtn.addEventListener('click', () => {
 
 closeBtn.addEventListener('click', () => {
     menu.classList.remove('open')
+});
+cancelBtn.addEventListener('click', () => {
+    contact.classList.remove('open-cont')
+});
+
+contactBtn.addEventListener('click', () => {
+    contact.classList.add('open-cont')
+   
+});
+
+contactBtn.addEventListener('touchstart', () => {
+    contact.classList.add('open-cont');
+  
 });
 
 // Close Menu when clicking outside
@@ -68,6 +91,64 @@ document.addEventListener('click', (e) => {
 function closeMenu() {
     menu.classList.remove('open')
 }
+
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+
+    document.querySelectorAll('.error-message').forEach((el) => el.remove());
+
+    const firstName = document.getElementById('first-name');
+    const email = document.getElementById('email');
+    const phone = document.getElementById('phone');
+    const message = document.getElementById('message');
+
+    let valid = true; 
+
+    if (firstName.value.trim() === '') {
+        const nameInput = form.querySelector('input[name="first-name"]');
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = 'Name is required!';
+        errorMessage.classList.add('error-message');
+        nameInput.after(errorMessage);
+        valid = false;
+    }
+
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (!email.value.trim().match(emailPattern)) {
+        const emailInput = form.querySelector('input[name="email"]');
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = 'Valid email is required!';
+        errorMessage.classList.add('error-message');
+        emailInput.after(errorMessage);
+        valid = false;
+    }
+
+    const phonePattern = /^[0-9]{10}$/;
+    if (!phone.value.trim().match(phonePattern)) {
+        const phoneInput = form.querySelector('input[name="phone"]');
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = 'A valid 10-digit phone number is required.';
+        errorMessage.classList.add('error-message');
+        phoneInput.after(errorMessage);
+        valid = false;
+    }
+
+    if (message.value.trim() === '') {
+        const messageInput = form.querySelector('textarea[name="message"]');
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = 'Message is required.';
+        errorMessage.classList.add('error-message');
+        messageInput.after(errorMessage);
+        valid = false;
+    }
+
+ 
+    if (!valid) {
+        e.preventDefault();
+    }
+});
+
 
 
 
